345f045943ac4c0c1ea1f21f3d5a8416ed29f763
sugandharuchir <ruchirsugandha@gmail.com>
2017-05-22 02:46:21 +0530

    change

--- harder/script.js
+++ harder/script.js
@@ -36,7 +36,7 @@ WARNING!!! WARNING!!!
 
 (function(window){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
+console.log(names);
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -51,18 +51,19 @@ for (var i = 0; i < names.length; i++) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = name.charAt(0);
-  firstLetter.toLowerCase();
+  var firstLetter = names[i].charAt(0);
+   firstLetter.toLowerCase();
   // console.log(firstLetter);
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-   if (firstLetter==='j') {
-      byeSpeaker.speak();
-   } else {
-    helloSpeaker.speak();
-   }
+    if (firstLetter==='J') 
+    {
+     byeSpeaker.speak(names[i]); }
+      else {
+     helloSpeaker.speak(names[i]);
+    }
 }
 }) (window);
